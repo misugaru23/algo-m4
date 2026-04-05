69e022f7dcef9065febbc9ca7dea3954b6ba1ee8
kirillshunaylov@gmail.com <kirillshunaylov@gmail.com>
2026-04-05 13:16:38 +0300

    github

--- github/main.js
+++ github/main.js
@@ -6,14 +6,42 @@ let load = document.getElementById("load")
 let dataCard;
 
 async function getCard(){
+    
     card.classList.add("none");
     load.classList.remove("none");
 
-    const response = await fetch ("https://api.github.com/users/KrasnovDaniil1")
+    const response = await fetch (`https://api.github.com/users/${searchValue.value}`);
+
     if (response.ok) {
         dataCard = await response.json()
         console.log(dataCard)
-    }
+        generateCard()
+    }  
+        load.classList.add("none")
+    
 }
 
 searchBtn.addEventListener("click",getCard)
+function generateCard() {
+    card.innerHTML = ` />
+            <img src="${dataCard.avatar_url}" />
+            <h1>${dataCard.login}</h1>
+            <p>${dataCard.bio}</p>
+            <nav>
+                <div>
+                    <i class="fas fa-map-marker-alt" style="color: yellow;"></i>
+                    <span>${dataCard.location}</span>
+                </div>
+                <div>
+                    <i class="fas fa-book" style="color: rgb(0, 255, 0);"></i>
+                    <span>${dataCard.public_repos}</span>
+                </div>
+                <div>
+                    <i class="fas fa-heart" style="color: rgb(255, 0, 0);"></i>
+                    <span>${dataCard.followers}</span>
+                </div>
+            </nav>
+            <a href="${dataCard.url}">Перейти</a>
+        `;
+            card.classList.remove("none");
+}
